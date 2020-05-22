926257d68e7050fcb661c3d78681d9abdc86a3aa
Adonis Puente <adonispuente@MacBook-Pro.local>
2020-05-22 11:58:00 -0400

    got color request

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,14 @@ import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import PrivateRoute from "./components/PrivateRoute";
 import "./styles.scss";
 import Login from "./components/Login";
+import BubblePage from "./components/BubblePage";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Switch>
-          <PrivateRoute
-            exact
-            path="/protectedFriends"
-            // component={FriendsList}
-          />
+          <PrivateRoute exact path="/bubbles" component={BubblePage} />
           <Route path="/login" component={Login} />
           <Route exact path="/" component={Login} />
         </Switch>
